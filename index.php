377a2220ec87c9c8bf36d45eaaecee04ae9ccb1f
root <root@web-server-1.c.team-brioche.internal>
2016-05-09 17:04:46 +0000

    Updated webapp, colors now and improved readability
    Signed-off-by: root <root@web-server-1.c.team-brioche.internal>

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
-<?php
-header('Content-type: text/plain');
-echo "Server IP: " . $_SERVER['SERVER_ADDR'];
-echo "\nCLient IP: " . $_SERVER['REMOTE_ADDR'];
-echo "\nX-Forwarded-for: " . $_SERVER['HTTP_X_FORWARDED_FOR'];
-echo "\nVersion: 9:46AM 5/9/2016"
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+    background-color:red;
+    color:white;
+}
+</style>
+</head>
 
+<body>
+<?php
+header('Content-type: text/html');
+echo "Backend Server IP: " . $_SERVER['SERVER_ADDR'];
+echo "<br/>Load Balancer IP: " . $_SERVER['REMOTE_ADDR'];
+echo "<br/>Your IP: " . $_SERVER['HTTP_X_FORWARDED_FOR'];
+echo "<br/>Version: 10:00AM 5/9/2016"
+?>
+</body>
+</html>
