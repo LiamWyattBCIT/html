52db4a51be70a382208634b5a5ec11a6ef857074
root <root@web-server-1.c.team-brioche.internal>
2016-05-09 16:47:05 +0000

    Updated
    Signed-off-by: root <root@web-server-1.c.team-brioche.internal>

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ header('Content-type: text/plain');
 echo "Server IP: " . $_SERVER['SERVER_ADDR'];
 echo "\nCLient IP: " . $_SERVER['REMOTE_ADDR'];
 echo "\nX-Forwarded-for: " . $_SERVER['HTTP_X_FORWARDED_FOR'];
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "\nVersion: 9:46AM 5/9/2016"
 ?>
 
